7d599359a1565dfbf23c1ec28cb1442fe6636a2b
Mohammad-imran75 <imranbd7531@gmail.com>
2024-03-22 00:27:49 +0600

    navbar added

--- src/Component/NavBar/NavBar.jsx
+++ src/Component/NavBar/NavBar.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import Linkid from "../LinkId/Linkid";
-
+import { HiOutlineMenuAlt1} from "react-icons/hi";
+import {RiCloseLine}  from 'react-icons/ri'
 
   
 const NavBar = () => {
+    const [open,setOpen] = useState(false)
     const routes = [
         { id: 1, path: '/', name: 'Home' },
         { id: 2, path: '/about', name: 'About' },
@@ -12,6 +15,14 @@ const NavBar = () => {
       ];
     return (
         <div className="flex">
+            <div className="text-2xl md:hidden" onClick={()=>setOpen(!open)}>
+                {
+                    open === true ?
+                    <HiOutlineMenuAlt1></HiOutlineMenuAlt1>:
+                    <RiCloseLine></RiCloseLine>
+                }
+            
+            </div>
             {
                 routes.map(route =><Linkid key={route.id} route={route}></Linkid>)
             }
